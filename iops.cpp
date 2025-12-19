101d657cb40a4ac1c092ae7a4f1f7016fa17877d
Aleksander Piętka <325637@stud.umk.pl>
2025-12-19 16:48:40 +0100

    Sprawdzanie, czy liczba jest nieujemna

--- iops.cpp
+++ iops.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main() {
     int x;
     cin >> x;
+    if (x < 0) {
+        cout << "Liczba nie moze byc ujemna!" << endl;
+        return 1;
+    }
     cout << x << endl;
     return 0;
 }
